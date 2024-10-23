0542ec7a12f15f9ca2584bf22d436eeef06d71b7
MateoRaul2121 <126632138+MateoRaul2121@users.noreply.github.com>
2024-10-23 12:48:53 -0600

    InsertCategoria

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,7 +2,10 @@ package handlers
 
 import (
 	"fmt"
+	"strconv"
 
+	"example.com/m/v2/auth"
+	"example.com/m/v2/routers"
 	"github.com/aws/aws-lambda-go/events"
 )
 
@@ -10,8 +13,88 @@ func Manejadores(path string, method string, body string, headers map[string]str
 
 	fmt.Println("Voy a procesar " + path + " > " + method)
 
-	//id := request.PathParameters["id"]
-	//idn, _ := strconv.Atoi(id)
+	id := request.PathParameters["id"]
+	idn, _ := strconv.Atoi(id)
+
+	isOk, statusCode, user := validoAuthorization(path, method, headers)
+	if !isOk {
+		return statusCode, user
+	}
+
+	fmt.Println("path[0:4] = " + path[0:4])
+
+	switch path[0:4] {
+	case "user":
+		return ProcesoUsers(body, path, method, user, id, request)
+	case "prod":
+		return ProcesoProducts(body, path, method, user, idn, request)
+	case "stoc":
+		return ProcesoStock(body, path, method, user, idn, request)
+	case "addr":
+		return ProcesoAddress(body, path, method, user, idn, request)
+	case "cate":
+		return ProcesoCategory(body, path, method, user, idn, request)
+	case "orde":
+		return ProcesoOrder(body, path, method, user, idn, request)
+	}
 
 	return 400, "Method invalid"
+}
+
+func validoAuthorization(path string, method string, headers map[string]string) (bool, int, string) {
+	if (path == "product" && method == "GET") ||
+		(path == "category" && method == "GET") {
+		return true, 200, ""
+	}
+
+	token := headers["authorization"]
+	if len(token) == 0 {
+		return false, 401, "Token requerido"
+	}
+
+	todoOK, err, msg := auth.ValidoToken(token)
+	if !todoOK {
+		if err != nil {
+			fmt.Println("Error en el token " + err.Error())
+			return false, 401, err.Error()
+		} else {
+			fmt.Println("Error en el token " + msg)
+			return false, 401, msg
+		}
+	}
+
+	fmt.Println("Token OK")
+	return true, 200, msg
+}
+
+func ProcesoUsers(body string, path string, method string, user string, id string, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
+}
+
+func ProcesoProducts(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
+}
+
+func ProcesoCategory(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+	switch method {
+	case "POST":
+		return routers.InsertCategory(body, user)
+	}
+	return 400, "Method Invalid"
+}
+
+func ProcesoStock(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+	return 400, "Method Invalid"
+}
+
+func ProcesoAddress(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
+}
+
+func ProcesoOrder(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
 }
